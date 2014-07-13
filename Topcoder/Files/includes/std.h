89dc202c4e8a86d9a22c5caa9190a7bbf9b28977
Niloo Ranjan <niloo.ranjan2005@gmail.com>
2014-07-13 19:43:26 -0400

    tc removed

--- Topcoder/Files/includes/std.h
+++
@@ -1,6 +0,0 @@
-
-#define talloc(ty, sz) (ty *) malloc ((sz) * sizeof(ty))
-#define copy_string(s) ((char *) strcpy(talloc(char, strlen(s)+1), s))
-#define TRUE 1
-#define FALSE 0
-#define fprintfv (void) fprintf
